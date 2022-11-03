4158760ec404ccc05d3614b51ffaa1ea52dd471e
cemolina29 <108438270+cemolina29@users.noreply.github.com>
2022-11-02 21:43:56 -0600

    adding Geojson

---
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -0,0 +1,76 @@
+// Add console.log to check to see if our code is working.
+//The console.log() function with the phrase "working" inside the parentheses will help us confirm that our logic.js file is 
+//being accessed in the console on Chrome.
+console.log("working");
+
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+      console.log(feature);
+      return L.marker(latlng);
+    }
+
+  }).addTo(map)
+
+  .bindPopup('Airport')
+//     .addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// // Get data from cities.js
+// let cityData = cities;
+
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location, {
+//         color: 'orange',
+//         fillColor: 'orange',
+//         fillOpacity:'0.20',
+//         radius: city.population/200000
+
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//     .addTo(map);
+// });
+
+//changing the marker to a point or dot. circle or ciclemarker
+// L.circleMarker([34.0522, -118.2437], {
+//     color: 'black',
+//     fillColor: '#ffffa1',
+//     radius: 300
+//  }).addTo(map);
+
+// We create the tile layer that will be the background of our map. To change back ground color change v# dark-v10 , streets-v11, satellite-streets-v11
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
